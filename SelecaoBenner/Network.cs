7fa6464549a64a31ff5258432648a75a1d2d7f3c
FaustoFnka <faustocrbl@gmail.com>
2020-06-05 18:39:20 -0300

    Inclusão de Comentários dos Códigos

--- SelecaoBenner/Network.cs
+++ SelecaoBenner/Network.cs
@@ -6,11 +6,13 @@ using System.Text;
 namespace SelecaoBenner
 {
     /// <summary>
-    /// CLASSE NetWork 
+    /// CLASSE: NetWork 
     /// Irá controlar uma rede de conexões entre um conjunto de pontos, 
-    /// onde poderá ser criado o conjunto, 
-    /// um método para criar conexões 
-    /// e permitirá a consulta se existe ou não conexão.
+    /// No construtor da classe será criado o conjunto de pontos a ser manipulado, 
+    /// um método para apresentar o consunto de ponto e suas conoexões
+    /// Outro método para criar conexões entre pontos
+    /// Mais um  método para desconectar pontos
+    /// e ainda permitirá a consulta se existe ou não conexão entre dois pontos.
     /// </summary>
     public class Network
     {
@@ -18,7 +20,7 @@ namespace SelecaoBenner
         public int nroElementos;
 
         /// <summary>
-        /// Construtor
+        /// CONSTRUTOR: 
         /// </summary>
         /// <param name="qtdElementos"> Quantidade de Elementos a ser criada pelo construtor</param>
         public Network(int qtdElementos)
@@ -26,27 +28,25 @@ namespace SelecaoBenner
             nroElementos = qtdElementos;
             ConjuntoElementos = new int[nroElementos, nroElementos];
 
-            //ShowNetwork();
-
-
             for (int i = 0; i < nroElementos; i++)
             {
                 for (int j = 0; j < nroElementos; j++)
                 {
                     if (i == j)
                     {
-                        ConjuntoElementos[i, j] = 1;
-                        //Console.WriteLine(String.Format("i= {0}, j= {1} = {2}", i, j, ConjuntoElementos[i, j]));
+                        ConjuntoElementos[i, j] = 1; //Faz a marcação de "conectado" = 1, caso seja o ponto com ele mesmo.
                     }
                     else
                     {
-                        ConjuntoElementos[i, j] = 0;
-                        //Console.WriteLine(String.Format("i= {0}, j= {1} = {2}", i, j, ConjuntoElementos[i, j]));
+                        ConjuntoElementos[i, j] = 0; //Para qualquer outro ponto na criação é marcado como "disconectado" = 0;
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// Methodo utilizado para imprimir a matriz de conexões.
+        /// </summary>
         public void ShowNetwork()
         {
             for (int i = 0; i < nroElementos; i++)
@@ -60,20 +60,40 @@ namespace SelecaoBenner
             }
         }
 
+        /// <summary>
+        /// Metodo usado para efetuar a conexão de dois pontos.
+        /// </summary>
+        /// <param name="NroOrigem">Ponto Origem</param>
+        /// <param name="NroDestino">Ponto Destinno</param>
         public void Connect(int NroOrigem, int NroDestino)
         {
             ConjuntoElementos[NroOrigem, NroDestino] = 1;
             ConjuntoElementos[NroDestino, NroOrigem] = 1;
         }
 
+        /// <summary>
+        /// Metodo usado para efetuar a desconeção de dois pontos
+        /// </summary>
+        /// <param name="NroOrigem">Ponto Origem</param>
+        /// <param name="NroDestino">Poto Destino</param>
         public void Disconnect(int NroOrigem, int NroDestino)
         {
             ConjuntoElementos[NroOrigem, NroDestino] = 0;
             ConjuntoElementos[NroDestino, NroOrigem] = 0;
         }
 
+        /// <summary>
+        /// Metodo utilizado para verificar se existe conexão entre os dois pontos. 
+        /// Irá chamar recursivamente a si mesmo para verificar todas as possibilidades de conexão.
+        /// </summary>
+        /// <param name="NroOrigem">Ponto Origem para análise</param>
+        /// <param name="NroDestino">Ponto de Destino para a análise</param>
+        /// <param name="iLoop"> indicador de Looping, só é necessário para chamadas recursivas do méthodo</param>
+        /// <returns></returns>
         public bool Query(int NroOrigem, int NroDestino, int iLoop = 0) // o ultimo parâmetro só será enviado caso seja chamada recursiva de dentro do methodo Query.
         {
+            // Se for Origem igual a Destino ou se for um ponto de conexão direto retornará True imediatamente, 
+            //Caso contrário irá validar a necessidade de chamada recursiva para validar caminho com conexões extras.
             if (ConjuntoElementos[NroOrigem, NroDestino] == 1 || NroOrigem == NroDestino)
             {
                 return true;
@@ -84,22 +104,27 @@ namespace SelecaoBenner
                 {
                     if (i != NroOrigem && i != NroDestino)
                     {
+                        //Chamada recursiva para analisar se quais as conexões diretas da Origem,
+                        //Só chamara o teste recursivo verificando com o destino se for conexão com a origem.
                         if (Query(NroOrigem, i, (i + 1)))
                         {
-                            Console.WriteLine(string.Format("O numero {0}, é ligado a {1}, verificando se {1} é ligado a {2}", NroOrigem, i, NroDestino));
+                            //Chamada recursiva para verificar se as conexões da Origem São "escalas" válidas para o destino.
                             return Query(i, NroDestino, i);
                         }
                         else
                         {
+                            //Caso a Origem não tiver conexão com o item verificado passa para o próximo item do for.
                             continue;
                         }
                     }
                     else
                     {
+                        //Caso o item do for analisado for igual a Origem ou ao Destino já verificados passa direto para o próximo item do for.
                         continue;
                     }
                 }
             }
+            //Caso nenhuma das tentativas de "escala" forem viáveis retorna falso, identificando como rota inviável.
             return false;
         }
 
--- SelecaoBenner/Program.cs
+++ SelecaoBenner/Program.cs
@@ -7,16 +7,24 @@ namespace SelecaoBenner
     {
         public static void Main()
         {
-            /*Console.WriteLine("Informe a quantidade de Itens:");
-            var qtdItens = int.Parse(Console.ReadLine());
-            */
+            //Para o exercídio foi removida a leitura do usuário
+            //Lendo do usuário tamanho da rede
+            //Console.WriteLine("Informe a quantidade de Itens:");
+            //var qtdItens = int.Parse(Console.ReadLine());
 
+            //Configurando o total de pontos do exercício
             int qtdItens = 8;
+
+            //Criando Rede
             var MinhaRede = new Network(qtdItens);
 
+            //Imprimindo rede inicial em tela
             Console.WriteLine("\nSua rede:");
+
+            //Apresenta em tela os pontos da rede e suas interconexões.
             MinhaRede.ShowNetwork();
 
+            //Faz a inclusão dos pontos do exercício.
             MinhaRede.Connect(0, 1);
             MinhaRede.Connect(0, 5);
             MinhaRede.Connect(1, 5);
@@ -24,13 +32,16 @@ namespace SelecaoBenner
             MinhaRede.Connect(4, 7);
             MinhaRede.Connect(4, 2);
 
+            //Irá apresentar a rede após as inclusões de conexão.
             Console.WriteLine("\nSua rede: ");
             MinhaRede.ShowNetwork();
 
-            Console.WriteLine("\nSua rede:");
+            //Faz a desconeção de dois pontos e imprime a rede novamente para validação.
             MinhaRede.Disconnect(4, 2);
+            Console.WriteLine("\nSua rede:");
             MinhaRede.ShowNetwork();
 
+            //Fas a impressão em tela de várias vavlidações de caminhos possíveis.
             Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 0, 1, MinhaRede.Query(0, 1)));
             Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 1, 0, MinhaRede.Query(0, 1)));
             Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 1, 3, MinhaRede.Query(1, 3)));
@@ -38,6 +49,9 @@ namespace SelecaoBenner
             Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 0, 3, MinhaRede.Query(0, 3)));
             Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 3, 0, MinhaRede.Query(3, 0)));
             Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 2, 0, MinhaRede.Query(2, 0)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 3, 4, MinhaRede.Query(3, 4)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 7, 0, MinhaRede.Query(7, 0)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 2, 6, MinhaRede.Query(2, 6)));
 
 
         }
