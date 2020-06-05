a3626b1cb52339fedbe9f978f219d5f79d356afe
FaustoFnka <faustocrbl@gmail.com>
2020-06-05 18:17:19 -0300

    Primeira Versão

---
+++ SelecaoBenner/Network.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SelecaoBenner
+{
+    /// <summary>
+    /// CLASSE NetWork 
+    /// Irá controlar uma rede de conexões entre um conjunto de pontos, 
+    /// onde poderá ser criado o conjunto, 
+    /// um método para criar conexões 
+    /// e permitirá a consulta se existe ou não conexão.
+    /// </summary>
+    public class Network
+    {
+        public int[,] ConjuntoElementos;
+        public int nroElementos;
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="qtdElementos"> Quantidade de Elementos a ser criada pelo construtor</param>
+        public Network(int qtdElementos)
+        {
+            nroElementos = qtdElementos;
+            ConjuntoElementos = new int[nroElementos, nroElementos];
+
+            //ShowNetwork();
+
+
+            for (int i = 0; i < nroElementos; i++)
+            {
+                for (int j = 0; j < nroElementos; j++)
+                {
+                    if (i == j)
+                    {
+                        ConjuntoElementos[i, j] = 1;
+                        //Console.WriteLine(String.Format("i= {0}, j= {1} = {2}", i, j, ConjuntoElementos[i, j]));
+                    }
+                    else
+                    {
+                        ConjuntoElementos[i, j] = 0;
+                        //Console.WriteLine(String.Format("i= {0}, j= {1} = {2}", i, j, ConjuntoElementos[i, j]));
+                    }
+                }
+            }
+        }
+
+        public void ShowNetwork()
+        {
+            for (int i = 0; i < nroElementos; i++)
+            {
+                for (int j = 0; j < nroElementos; j++)
+                {
+                    //Console.WriteLine(String.Format("[{0}] [{1}] = {2}",i,j, ConjuntoElementos[i,j]));
+                    Console.Write(String.Format("{0} ", ConjuntoElementos[i, j]));
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public void Connect(int NroOrigem, int NroDestino)
+        {
+            ConjuntoElementos[NroOrigem, NroDestino] = 1;
+            ConjuntoElementos[NroDestino, NroOrigem] = 1;
+        }
+
+        public void Disconnect(int NroOrigem, int NroDestino)
+        {
+            ConjuntoElementos[NroOrigem, NroDestino] = 0;
+            ConjuntoElementos[NroDestino, NroOrigem] = 0;
+        }
+
+        public bool Query(int NroOrigem, int NroDestino, int iLoop = 0) // o ultimo parâmetro só será enviado caso seja chamada recursiva de dentro do methodo Query.
+        {
+            if (ConjuntoElementos[NroOrigem, NroDestino] == 1 || NroOrigem == NroDestino)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = iLoop; i < nroElementos; i++)
+                {
+                    if (i != NroOrigem && i != NroDestino)
+                    {
+                        if (Query(NroOrigem, i, (i + 1)))
+                        {
+                            Console.WriteLine(string.Format("O numero {0}, é ligado a {1}, verificando se {1} é ligado a {2}", NroOrigem, i, NroDestino));
+                            return Query(i, NroDestino, i);
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ SelecaoBenner/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SelecaoBenner
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            /*Console.WriteLine("Informe a quantidade de Itens:");
+            var qtdItens = int.Parse(Console.ReadLine());
+            */
+
+            int qtdItens = 8;
+            var MinhaRede = new Network(qtdItens);
+
+            Console.WriteLine("\nSua rede:");
+            MinhaRede.ShowNetwork();
+
+            MinhaRede.Connect(0, 1);
+            MinhaRede.Connect(0, 5);
+            MinhaRede.Connect(1, 5);
+            MinhaRede.Connect(1, 3);
+            MinhaRede.Connect(4, 7);
+            MinhaRede.Connect(4, 2);
+
+            Console.WriteLine("\nSua rede: ");
+            MinhaRede.ShowNetwork();
+
+            Console.WriteLine("\nSua rede:");
+            MinhaRede.Disconnect(4, 2);
+            MinhaRede.ShowNetwork();
+
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 0, 1, MinhaRede.Query(0, 1)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 1, 0, MinhaRede.Query(0, 1)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 1, 3, MinhaRede.Query(1, 3)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 3, 1, MinhaRede.Query(1, 3)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 0, 3, MinhaRede.Query(0, 3)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 3, 0, MinhaRede.Query(3, 0)));
+            Console.WriteLine(string.Format("\n{0} está conectado a {1}? {2}", 2, 0, MinhaRede.Query(2, 0)));
+
+
+        }
+    }
+}
